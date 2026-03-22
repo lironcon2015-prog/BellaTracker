894e9f536e5c2ea8b4e954d863e4f4665820cd2f
lironcon2015-prog <lironcon2015@gmail.com>
2026-03-22 22:35:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * העלה את CACHE_VERSION בכל עדכון קוד.
  */
 
-const CACHE_VERSION = 'gymstart-v1.8.2-3';
+const CACHE_VERSION = 'gymstart-v1.8.2-4';
 
 const FILES_TO_CACHE = [
     './index.html',
